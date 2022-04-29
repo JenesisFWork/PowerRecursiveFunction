432dab238d66a359cf4668599a64449d780d3a03
JenesisFWork <jenesisfablework@gmail.com>
2022-04-28 18:01:40 -0700

    Working on the program
    Want this program to say "Hello World" and "Goodbye World".

---
+++ demo/src/main/java/recursion/PowerRecursionFunction.java
@@ -0,0 +1,23 @@
+/* A program that uses recursive function (x^n).
+
+    Programmer: Jenesis Fabia
+    Class: Data Structures
+    Date: April 28, 2022
+
+    Inspired from a Youtube video (link below):
+    https://www.youtube.com/watch?v=sE0sH8vSmE4
+*/
+
+
+package recursion;
+
+public class PowerRecursionFunction {
+    
+    public static void main (String [] args) {
+        sayHelloWorld();
+    }
+
+    public static void sayHelloWorld() {
+        System.out.println("Hello World!");
+    }
+}
